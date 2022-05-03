19b925aa89a606e21dc7c8c2a949f481bbdb7355
kiri-91 <kirifog91@gmail.com>
2022-05-03 13:35:39 +0900

    create ProfileDescription

--- app/src/main/java/com/example/portfolio/ProfileScreen.kt
+++ app/src/main/java/com/example/portfolio/ProfileScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -98,6 +100,14 @@ fun ProfileSection(
             Spacer(modifier = Modifier.width(16.dp))
             StatSection(modifier = Modifier.weight(7f))
         }
+        ProfileDescription(
+            displayName = "Kazuma Kiriyama",
+            description = "University of Aizu student.\n" +
+                "I am a junior developer and marketer.",
+            url = "https://github.com/kiri-91",
+            followedBy = listOf("jindogg", "lex"),
+            otherCount = 21
+        )
     }
 }
 
@@ -155,6 +165,67 @@ fun ProfileStat(
         Text(text = text)
     }
 }
+
+@Composable
+fun ProfileDescription(
+    displayName: String,
+    description: String,
+    url: String,
+    followedBy: List<String>,
+    otherCount: Int
+) {
+    val letterSpacing = 0.5.sp
+    val lineHeight = 20.sp
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = displayName,
+            fontWeight = FontWeight.Bold,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            text = description,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            text =url,
+            color = Color(0xFF3D3D91),
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            // 1 つのテキストに複数のスタイルを設定する
+            text = buildAnnotatedString {
+                val boldStyle = SpanStyle(
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+                append("Followed by ")
+                followedBy.forEachIndexed { index, name ->
+                    pushStyle(boldStyle)
+                    append(name)
+                    pop()
+                    if (index < followedBy.size - 1) {
+                        append(", ")
+                    }
+                }
+                if (otherCount > 2) {
+                    append(" and ")
+                    pushStyle(boldStyle)
+                    append("$otherCount others")
+                }
+            },
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+    }
+}
+
 
 @Preview
 @Composable
